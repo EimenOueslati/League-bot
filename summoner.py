488897df63dd2d9905173c9ab9f872f7a5432552
Eimen Ouesalti <aymanoueslati22@gmail.com>
2023-04-29 20:00:19 +0200

    Created the classes to be used to parse the API response.

---
+++ summoner.py
@@ -0,0 +1,18 @@
+#A class that represents a summoner
+class Summoner:
+    def __init__(self, summonerName, flex, soloQ):
+        self.summonerName = summonerName
+        self.flex = flex
+        self.soloQ = soloQ
+
+
+
+#A class that represents a summoners stats related to a game type (flex/soloQ)
+class ranked:
+     def __init__(self, tiere, rank, leaguePoints, wins, losses,):
+         self.tiere = tiere
+         self.rank = rank
+         self.leaguePoints = leaguePoints
+         self.wins = wins
+         self.losses = losses
+
